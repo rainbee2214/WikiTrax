b08f5b33ddc7ae2ffc2c10502033b1d3a3235057
rainbee2214 <rainbee2214@gmail.com>
2015-03-26 22:55:56 -0300

    Finished some more tutorial d3 examples,

--- js/graphs.js
+++ js/graphs.js
@@ -7,59 +7,108 @@ function OnWindowLoad()
 
 function DrawGraph(dataset)
 {
-	var dataset = [
-                  [ 5,     20 ],
-                  [ 480,   90 ],
-                  [ 250,   50 ],
-                  [ 100,   33 ],
-                  [ 330,   95 ],
-                  [ 410,   12 ],
-                  [ 475,   44 ],
-                  [ 25,    67 ],
-                  [ 85,    21 ],
-                  [ 220,   88 ]
-              ];
 
- 	var w = 500;
- 	var h = 100;
- 	var barPadding = 1; 
+	//Scatter plot with random data, x axis and y axis
+ // 	var w = 500;
+ // 	var h = 300;
+	// // scale(2.5);
+	// // scale.domain([100,500])
+	// // scale.range([10,350])
 
- 	var svg = d3.select("body")
- 	            .append("svg")
- 	            .attr("width", w)
- 	            .attr("height", h);
+	// // var dataset = [
+	// // 	[ 5,     20 ],
+	// // 	[ 480,   90 ],
+	// // 	[ 250,   50 ],
+	// // 	[ 100,   33 ],
+	// // 	[ 330,   95 ],
+	// // 	[ 410,   12 ],
+	// // 	[ 475,   44 ],
+	// // 	[ 25,    67 ],
+	// // 	[ 85,    21 ],
+	// // 	[ 220,   88 ],
+	// // 	[600, 150]
+	// // 	];
+	// var dataset = [];
+	// var numDataPoints = 50;
+	// var xRange = Math.random() * 1000;
+	// var yRange = Math.random() * 1000;
+	// for (var i = 0; i < numDataPoints; i++) {
+	//     var newNumber1 = Math.round(Math.random() * xRange);
+	//     var newNumber2 = Math.round(Math.random() * yRange);
+	//     dataset.push([newNumber1, newNumber2]);
+	// }
 
- 	svg.selectAll("circle")
-	   .data(dataset)
-	   .enter()
-	   .append("circle")  
-	   .attr("cx", function(d) {
-	           return d[0];
-	      })
-	      .attr("cy", function(d) {
-	           return d[1];
-	      })
-	      .attr("r", 5)
-	      .attr("r", function(d) {
-	          return Math.sqrt(h - d[1]);
-	      });        
+ // 	var padding = 30; 
+	// var xScale = d3.scale.linear()
+	// 				.domain([0, d3.max(dataset, function(d) { return d[0]; })])
+	// 				.range([padding, w-padding*2]);
 
-	      svg.selectAll("text")
-	         .data(dataset)
-	         .enter()
-	         .append("text")
-	         .text(function(d) {
-	                 return d[0] + "," + d[1];
-	            })
-	         .attr("x", function(d) {
-	                 return d[0];
-	            })
-	            .attr("y", function(d) {
-	                 return d[1];
-	            })
-	            .attr("font-family", "sans-serif")
-	              .attr("font-size", "11px")
-	              .attr("fill", "red");
+	// var yScale = d3.scale.linear()
+	// 				.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+	// 				.range([h-padding, padding]);
+
+	// var rScale = d3.scale.linear()
+	// 				.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+	// 				.range([2, 5]);
+
+	// var formatAsPercentage = d3.format(".1%");
+	// var xAxis = d3.svg.axis()
+	// 				.scale(xScale)
+	// 				.orient("bottom")
+	// 				.ticks(8);
+
+	// var yAxis = d3.svg.axis()
+	//                   .scale(yScale)
+	//                   .orient("left")
+	//                   .ticks(5);
+
+	// var svg = d3.select("body")
+	// 			.append("svg")
+	// 			.attr("width", w)
+	// 			.attr("height", h);
+
+	// svg.selectAll("circle")
+	// 	.data(dataset)
+	// 	.enter()
+	// 	.append("circle")  
+	// 	.attr("cx", function(d) {
+	// 		return xScale(d[0]);
+	// 	})
+	// 	.attr("cy", function(d) {
+	// 		return yScale(d[1]);
+	// 	})
+	// 	.attr("r", function(d) 
+	// 	{
+	//     	return rScale(d[1]);
+	// 	});        
+
+	// svg.selectAll("text")
+		// .data(dataset)
+		// .enter()
+		// .append("text")
+		// .text(function(d) {
+		// 	return d[0] + "," + d[1];
+		// })
+		// .attr("x", function(d) {
+		// 	return xScale(d[0]);
+		// })
+		// .attr("y", function(d) {
+		// 	return yScale(d[1]);
+		// })
+		// .attr("font-family", "sans-serif")
+		// .attr("font-size", "11px")
+		// .attr("fill", "red");
+
+
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + (h - padding) + ")")
+	    .call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "axis")
+	    .attr("transform", "translate(" + padding + ",0)")
+	    .call(yAxis);
 	//BAR CHARTS
  // //	var dataset = [5, 10, 15, 20, 25, 25, 20, 15, 10, 5];
  // 	var dataset = [];                        //Initialize empty array
